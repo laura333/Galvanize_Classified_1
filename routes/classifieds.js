95bec7fea5ebd54e9b5fe1d88ef5f867d3c2ceef
Laura Baukol <laura.baukol@mac.com>
2017-01-30 11:16:20 -0700

    get and post routes working, tests passing for those routes

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const express = require('express');
@@ -6,5 +5,43 @@ const knex = require('../knex');
 const router = express.Router();
 
 // YOUR CODE HERE
+router.get('/', (req, res, next) => {
+    knex('classifieds')
+        .select('id', 'title', 'description', 'price', 'item_image')
+        .then((rows) => {
+            res.send(rows);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+router.get('/:id', (req, res, next) => {
+    var id = Number.parseInt(req.params.id);
+    if (Number.isNaN(id)) {
+        return next();
+    }
+    knex('classifieds')
+        .select('id', 'title', 'description', 'price', 'item_image')
+        .where('id', id)
+        .then((row) => {
+            res.send(row[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+router.post('/', (req, res, next) => {
+    var { title, description, price, item_image } = req.body;
+    knex('classifieds')
+        .insert({ title, description, price, item_image }, ['id', 'title', 'description', 'price', 'item_image'])
+        .then((row) => {
+            res.send(row[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
 
 module.exports = router;
